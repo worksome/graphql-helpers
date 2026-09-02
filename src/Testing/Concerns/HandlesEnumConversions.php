7efede46e042b2dba291e672de16defc73c57b85
Owen Voke <development@voke.dev>
2026-09-02 11:57:25 +0100

    chore: ignore in PHPStan config

--- src/Testing/Concerns/HandlesEnumConversions.php
+++ src/Testing/Concerns/HandlesEnumConversions.php
@@ -7,7 +7,6 @@ namespace Worksome\GraphQLHelpers\Testing\Concerns;
 use UnitEnum;
 use Worksome\GraphQLHelpers\Utils\UpperSnakeCaseConverter;
 
-/** @phpstan-ignore trait.unused */
 trait HandlesEnumConversions
 {
     public function enumToGraphQL(UnitEnum $enum): string
